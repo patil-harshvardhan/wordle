5da5253b873565c1f8f950eeb56294550bb52755
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2025-03-30 22:06:29 +0100

    trail

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 
 export function App() {
-
-  const [word,setWord] = useState<string| null>(null)
 
   const getWord = async () : Promise<void> => {
     const response = await fetch(
